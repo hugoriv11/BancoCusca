d684db4e3a8e4ed3b9a64c9531cca9fbb06cfa61
Hugo Jr. <00161417@uca.edu.sv>
2018-06-06 08:08:08 -0600

    base

---
+++ src/banco/Cajero.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+/**
+ *
+ * @author HugoJr. <Hugo Rivera at 00161417@uca.edu.sv>
+ */
+public class Cajero {
+    private String nombre;
+
+    public Cajero(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void procesarTransaccion(Cliente cliente, long timeStamp) {
+
+		System.out.println("La cajera " + this.nombre + 
+				" COMIENZA A PROCESAR LA COMPRA DEL CLIENTE " + cliente.getNombre() + 
+				" EN EL TIEMPO: " + (System.currentTimeMillis() - timeStamp) / 1000	+
+				"seg");
+
+		for (int i = 0; i < cliente.getCarroCompra().length; i++) { 
+				this.esperarXsegundos(cliente.getCarroCompra()[i]); 
+				System.out.println("Procesado el producto " + (i + 1) +  
+				" ->Tiempo: " + (System.currentTimeMillis() - timeStamp) / 1000 + 
+				"seg");
+		}
+
+		System.out.println("La cajera " + this.nombre + " HA TERMINADO DE PROCESAR " + 
+				cliente.getNombre() + " EN EL TIEMPO: " + 
+				(System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+
+	}
+
+
+	private void esperarXsegundos(int segundos) {
+		try {
+			Thread.sleep(segundos * 1000);
+		} catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
+	}
+}
---
+++ src/banco/Cliente.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+/**
+ *
+ * @author HugoJr. <Hugo Rivera at 00161417@uca.edu.sv>
+ */
+public class Cliente {
+    private String nombre;
+    private int[] transacciones;
+
+    public Cliente(String nombre, int[] transacciones) {
+        this.nombre = nombre;
+        this.transacciones = transacciones;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int[] getTransacciones() {
+        return transacciones;
+    }
+
+    public void setTransacciones(int[] transacciones) {
+        this.transacciones = transacciones;
+    }
+    
+    
+}
