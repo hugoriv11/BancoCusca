f4789457473de723b1b327e60ba096a0fffa1816
Hugo Jr. <00161417@uca.edu.sv>
2018-06-06 08:13:03 -0600

    BASE banquito cuscatlan

--- src/banco/Cajero.java
+++ src/banco/Cajero.java
@@ -26,19 +26,19 @@ public class Cajero {
     
     public void procesarTransaccion(Cliente cliente, long timeStamp) {
 
-		System.out.println("La cajera " + this.nombre + 
-				" COMIENZA A PROCESAR LA COMPRA DEL CLIENTE " + cliente.getNombre() + 
+		System.out.println("El cajera " + this.nombre + 
+				" COMIENZA A PROCESAR LA TRANSSACCION DEL CLIENTE " + cliente.getNombre() + 
 				" EN EL TIEMPO: " + (System.currentTimeMillis() - timeStamp) / 1000	+
 				"seg");
 
-		for (int i = 0; i < cliente.getCarroCompra().length; i++) { 
-				this.esperarXsegundos(cliente.getCarroCompra()[i]); 
-				System.out.println("Procesado el producto " + (i + 1) +  
+		for (int i = 0; i < cliente.getTransacciones().length; i++) { 
+				this.esperarXsegundos(cliente.getTransacciones()[i]); 
+				System.out.println("Procesado la transaccion " + (i + 1) +  
 				" ->Tiempo: " + (System.currentTimeMillis() - timeStamp) / 1000 + 
 				"seg");
 		}
 
-		System.out.println("La cajera " + this.nombre + " HA TERMINADO DE PROCESAR " + 
+		System.out.println("El cajero " + this.nombre + " HA TERMINADO DE PROCESAR " + 
 				cliente.getNombre() + " EN EL TIEMPO: " + 
 				(System.currentTimeMillis() - timeStamp) / 1000 + "seg");
 
